febf76f18dc4282c4d6125b166084cac3766f913
Unknown <cyrus135@hotmail.fr>
2018-07-25 10:21:05 +0200

    Added tutorial

---
+++ Assets/Scripts/TutorialMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TutorialMenu : MonoBehaviour {
+
+	void OnEnable(){
+		StartCoroutine(hideTutorial());
+	}
+
+	IEnumerator hideTutorial(){
+		yield return new WaitForSeconds (10);
+		gameObject.SetActive(false);
+		StopAllCoroutines();
+	}
+}
